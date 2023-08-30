fdf84c91f99e24546c1dc0a925cf6f5c5c056d90
abdesselam <fa_belkhiri@esi.dz>
2023-08-30 16:26:25 +0000

    adress ip

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-export const BASE_URL ="http://localhost:3005/api/"
+export const BASE_URL ="http://tb-dns.canadacentral.cloudapp.azure.com:3005/api"
 export const SOCKET_URL = 'http://localhost:3005/ws/';
 export const INTERVAL = 5000;
 
